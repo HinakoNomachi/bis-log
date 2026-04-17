220e13a57e8cd653dd09eea2598823cfd29c610a
Hikaru Kimura <>
2026-04-17 19:44:34 +0900

    prettier

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
-export const blogsTable = pgTable("blogs", {
+export const blogsTable = pgTable('blogs', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   title: varchar({ length: 255 }).notNull(),
 });
